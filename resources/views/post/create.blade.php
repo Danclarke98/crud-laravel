282de805c98d01d77ff6a31a8d6d601314334b35
Danclarke98 <danclarke98@gmail.com>
2018-11-14 07:17:09 +0000

    Slight UI tweaks to the create page

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -3,7 +3,8 @@
 <div class='container'>
     <div class= "row">
         <div class="col-lg-12">
-            <h3>New Post</h3>
+            <h3>Create a new post</h3>
+            <br>
 
         </div>
     </div>
@@ -20,25 +21,29 @@
         </div>
     @endif
         <form action="{{ route('post.store') }}" method="post">
+            {{-- Authentication --}}
             @csrf
+            {{-- order page in grid layout and title/text input boxes --}}
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-6">
                 <strong>
                     Title
                 </strong>
-                <input type="text" name="title" class="form-control" placeholder="title">
+                <input type="text" name="title" class="form-control" placeholder="Enter Title Here">
             </div>
 
-            <div class="col-md-12">
+            <div class="col-md-10">
                     <strong>
                         Content
                     </strong>
-                    <textarea class="form-control" placeholder="content" name="content" id="" cols="100" rows="10"></textarea>
+                    <textarea class="form-control" placeholder="Type your post here" name="content" id="" cols="100" rows="25"></textarea>
                 </div>
-                <div>
+                <div class="col-md-12">
+                    <br>
                     <a href="{{ route('post.index') }}" class="btn btn-sm btn-success">Back</a>
                     <button type="submit" class="btn btn-sm btn-primary">Submit</button>
                 </div>
+                
         </div>
       
                 
